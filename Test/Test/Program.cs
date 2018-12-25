9eff323eecff51451fdd75ef7681b22d98dc1557
anhmairora <anhmai.rora@gmail.com>
2018-12-25 10:46:30 +0700

    Init project

---
+++ Test/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RectangleApplication 
+{
+    class Rectangle
+    {
+        float length;
+        float width;
+        public void Acceptdetails()
+        {
+            length = 4.5f;
+            width = 3.5f;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+
+        }
+    }
+    class ExecutteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
