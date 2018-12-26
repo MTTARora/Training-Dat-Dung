d9fbae5cba40faa5a0081480302f84e67f5abd30
HoangDung <phamhoangdung4@gmail.com>
2018-12-26 20:43:14 +0700

    em update project nó còn 1 số lỗi em đang fix

---
+++ Project3/Project3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int:  {0}", sizeof(int));
+            Console.ReadLine();
+        }
+    }
+}
