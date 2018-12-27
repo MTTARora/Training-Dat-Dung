228c90221d86ce972f3572f1aacec87bb3d80529
HoangDung <phamhoangdung4@gmail.com>
2018-12-27 16:09:13 +0700

    Array- Foreach

---
+++ Array-Foreach/Array-Foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[20];
+            for (int i = 0; i < 20; i++) 
+                a[i] = i + 20;
+            foreach(int j in a)
+            {
+                int i = j - 20;
+                Console.WriteLine("Phan tu [{0}]= {1}", i, j);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
