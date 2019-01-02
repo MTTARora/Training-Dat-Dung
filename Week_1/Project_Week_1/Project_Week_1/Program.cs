4286861850ebfeb38a2f38b6b67c8a0a49d8eb0c
HoangDung <phamhoangdung4@gmail.com>
2019-01-02 16:48:17 +0700

    week1

---
+++ Week_1/Project_Week_1/Project_Week_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Project_Week_1.Model;
+
+namespace Project_Week_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserModel u = new Model.UserModel();
+            Model.Display d = new Model.Display();
+            d.disPlaySign();
+            u.Sign();
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
