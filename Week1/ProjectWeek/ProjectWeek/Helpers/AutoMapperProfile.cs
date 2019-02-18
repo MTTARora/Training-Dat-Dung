7b78ccbca8fc22d9ba6fdab9df83025e30fe4777
HoangDung <phamhoangdung4@gmail.com>
2019-02-18 21:58:27 +0700

    [TDD-50] add User

---
+++ Week1/ProjectWeek/ProjectWeek/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using ProjectWeek.Models;
+using ProjectWeek.Dtos;
+
+namespace ProjectWeek.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
