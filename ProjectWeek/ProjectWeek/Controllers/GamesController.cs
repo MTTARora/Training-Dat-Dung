731d63eb241028a5c64ef61d3b0477aa7546d0e5
HoangDung1 <phamhoangdung4@gmail.com>
2019-02-28 10:32:53 +0700

    [TDD-50] add GameList

---
+++ ProjectWeek/ProjectWeek/Controllers/GamesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GamesDataAccess;
+
+namespace ProjectWeek.Controllers
+{
+    public class GamesController : ApiController
+    {
+        public IEnumerable<GamesDB> GetAllGame()
+        {
+            using (GamesEntities entities = new GamesEntities())
+            {
+                return entities.GamesDBs.ToList();
+            }
+        }
+
+        public GamesDB GetGame(int id)
+        {
+            using (GamesEntities entities = new GamesEntities())
+            {
+                return entities.GamesDBs.FirstOrDefault(p => p.Id == id);
+            }
+        }
+    }
+
+}
---
+++ ProjectWeek/ProjectWeek/Models/Games.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectWeek.Models
+{
+    public class Games
+    {
+        public int Id { get; set; }
+        [Required] [Key]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
